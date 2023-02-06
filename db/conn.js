0c0bbff0e6fde2fb7f7108daff7aa33aba7bff58
Mikael <mikaelkawam.dev@gmail.com>
2023-02-06 16:08:39 -0300

    connect to cloud db

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 async function main() {
-    await mongoose.connect("mongodb+srv://book:book@cluster0.xgwsnin.mongodb.net/?retryWrites=true&w=majority')
+    await mongoose.connect("mongodb+srv://book1:book1@cluster0.xgwsnin.mongodb.net/?retryWrites=true&w=majority')
 }
 
 main().catch((err) => console.log(err))
